ee91c9ca2a5283f4a8b053906008339d23b7ab42
foobardrum <tobias@eggut.ch>
2019-03-09 17:03:21 +0100

    removed unused dependencies

--- src/main/java/ch/uzh/ifi/seal/soprafs19/exception/NotAuthorizedException.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs19/exception/NotAuthorizedException.java
@@ -1,9 +1,6 @@
 package ch.uzh.ifi.seal.soprafs19.exception;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.server.ResponseStatusException;
 
 public class NotAuthorizedException extends ResponseStatusException {
