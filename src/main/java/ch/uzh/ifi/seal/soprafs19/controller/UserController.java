a2f00885ac5ba8a09fbd4e21165ac3e73ae3f870
foobardrum <tobias@eggut.ch>
2019-03-09 09:52:16 +0100

    removed obsolete imports

--- src/main/java/ch/uzh/ifi/seal/soprafs19/controller/UserController.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs19/controller/UserController.java
@@ -1,13 +1,9 @@
 package ch.uzh.ifi.seal.soprafs19.controller;
 
 import ch.uzh.ifi.seal.soprafs19.entity.User;
-import ch.uzh.ifi.seal.soprafs19.exception.NotAuthorizedException;
-import ch.uzh.ifi.seal.soprafs19.exception.UserNotFoundException;
-import ch.uzh.ifi.seal.soprafs19.repository.UserRepository;
 import ch.uzh.ifi.seal.soprafs19.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class UserController {
